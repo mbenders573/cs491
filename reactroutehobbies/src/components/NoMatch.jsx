64b392f6bca2fa4b70957cd7226f1d47445b813b
Mark Benders <bendersmark573@gmail.com>
2025-10-13 18:20:56 -0400

    made navigation with router

---
+++ reactroutehobbies/src/components/NoMatch.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../App.css';
+import {  } from 'react-bootstrap';
+
+const NoMatch = () => {
+    return (
+        <>
+            <h1>404: Page not found</h1>
+        </>
+    );
+};
+
+export default NoMatch;
