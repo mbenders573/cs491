dbed8a33a7b2f0c29c70f9e692097fb08934a11a
Mark Benders <bendersmark573@gmail.com>
2025-10-13 18:45:13 -0400

    styled nav with bootstrap

--- reactroutehobbies/src/App.jsx
+++ reactroutehobbies/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route, Link } from 'react-router-dom';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import About from './components/About.jsx'
 import Hobbies from './components/Hobbies.jsx'
 import Home from './components/Home.jsx'
@@ -11,12 +12,19 @@ import NoMatch from './components/NoMatch.jsx'
 function App() {
   return (
     <>
-
-      <nav>
-        <Link to = "/">Home</Link>{" "}|{" "}
-        <Link to = "/hobbies">Hobbies</Link>{" "}|{" "}
-        <Link to = "/about">About</Link>
-      </nav>
+      <Navbar bg = "light" fixed = "top" className = "justify-content-center">
+        <Nav variant="underline">
+          <Nav.Item>
+            <Nav.Link as = {Link} to = "/" className = "text-dark">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as = {Link} to = "/hobbies" className = "text-dark">Hobbies</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as = {Link} to = "/about" className = "text-dark">About</Nav.Link>
+          </Nav.Item>
+        </Nav>  
+      </Navbar>
 
       <Routes>
         <Route path = "/" element = {<Home/>}>
