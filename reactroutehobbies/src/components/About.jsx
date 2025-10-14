fe57ad23cd5de12107a8d7b1b405f2321602f5e2
Mark Benders <bendersmark573@gmail.com>
2025-10-14 10:35:42 -0400

    made about

--- reactroutehobbies/src/components/About.jsx
+++ reactroutehobbies/src/components/About.jsx
@@ -1,10 +1,26 @@
 import '../App.css';
-import {  } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 
 const About = () => {
     return (
         <>
-            <h1>About</h1>
+            <h1>About Me</h1>
+            <div className = "text-start">
+                <h3>
+                    Github:{" "}
+                    <a href = "https://github.com/mbenders573" target = "_blank" className = "text-decoration-none">
+                        https://github.com/mbenders573
+                    </a>
+                </h3>
+                <h3>
+                    Email: mb03756n@pace.edu
+                </h3>
+                <p>
+                    I am Mark, a Senior at Pace. I enjoy puzzles, and math. <br/>
+                    Being new to react, I hope to learn a lot while completing the project.
+                </p>
+            </div>
+            
         </>
     );
 };
